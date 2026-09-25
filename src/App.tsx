f7cd302d8a3f7b411f29d70184c7bdeb80c00c19
Izumi Takayuki <r8quattrospider@gmail.com>
2026-09-25 17:37:58 +0900

    style: Project Adjustment Design

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,10 @@ import FormPage from "./pages/FormPage";
 function App() {
   return (
     <>
-      <h1>5.1.2.React-Form-Input-Value</h1>
-      <FormPage />
+      <div className="min-h-screen bg-gray-800 flex flex-col justify-center">
+        <h1>5.1.2.React-Form-Input-Value</h1>
+        <FormPage />
+      </div>
     </>
   );
 }
